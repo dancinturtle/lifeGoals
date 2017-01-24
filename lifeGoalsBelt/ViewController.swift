1239d123f1e5b8ac74b3ce1243b92fef131bffea
dancinturtle <amy.giver@gmail.com>
2017-01-24 15:50:20 -0800

    Initial Commit

---
+++ lifeGoalsBelt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lifeGoalsBelt
+//
+//  Created by Amy Giver on 1/24/17.
+//  Copyright © 2017 Amy Giver Squid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
